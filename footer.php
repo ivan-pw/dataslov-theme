15add92d7c55f18262fc35c2126af721822df0d3
Ivan Zviagin <ivan@ivan.pw>
2022-12-07 14:41:03 +0400

    update share buttons

--- footer.php
+++ footer.php
@@ -2,38 +2,45 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-3">
-                <a class="logo" href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.svg" /></a>
+                <a class="logo" href="/"><img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.svg" /></a>
                 <div class="contacts">
                     <ul>
-                        <li><a href="mailto:info@dataslov.ru"><i class="fa fa-envelope" aria-hidden="true"></i> <span> info@dataslov.ru</span></a></li>
-                        <li><a href="tel:8(812)363-61-11"><i class="fa fa-phone" aria-hidden="true"></i><span> 8(812)363-61-11, доб. 3429</span></a></li>
-                        <li><i class="fa fa-map-marker" aria-hidden="true"></i><span>Санкт-Петербург, <br />1-я линия В.О., <br />д. 26, к. 703</span></li>
-                        <li><a href="https://vk.com/dataslov" target="_blank"><i class="fa  fa-vk" aria-hidden="true"></i><span class="mt-1">VK<br /></span></a></li>
+                        <li><a href="mailto:info@dataslov.ru"><i class="fa fa-envelope" aria-hidden="true"></i> <span>
+                                    info@dataslov.ru</span></a></li>
+                        <li><a href="tel:8(812)363-61-11"><i class="fa fa-phone" aria-hidden="true"></i><span>
+                                    8(812)363-61-11, доб. 3429</span></a></li>
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i><span>Санкт-Петербург, <br />1-я линия
+                                В.О., <br />д. 26, к. 703</span></li>
+                        <li><a href="https://vk.com/dataslov" target="_blank"><i class="fa  fa-vk"
+                                    aria-hidden="true"></i><span class="mt-1">VK<br /></span></a></li>
+                        <li><a href="https://t.me/dataslov" target="_blank"><i class="fa  fa-telegram"
+                                    aria-hidden="true"></i><span class="mt-1">Telegram<br /></span></a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-12 col-md-3">
                 <?php
 
-                wp_nav_menu([
-                    'theme_location' => 'footer_menu',
-                    'menu_id' => 'footer-menu',
-                    'depth' => 2,
-                    'container' => false,
-                    'menu_class' => 'menu menu-1',
-                ]);?>
+wp_nav_menu([
+    'theme_location' => 'footer_menu',
+    'menu_id' => 'footer-menu',
+    'depth' => 2,
+    'container' => false,
+    'menu_class' => 'menu menu-1',
+]);?>
 
             </div>
             <div class="col-12 col-md-3">
                 <?php
 
-                wp_nav_menu([
-                    'theme_location' => 'footer_menu_2',
-                    'menu_id' => 'footer-menu_2',
-                    'depth' => 2,
-                    'container' => false,
-                    'menu_class' => 'menu menu-2',
-                ]);?>
+wp_nav_menu([
+    'theme_location' => 'footer_menu_2',
+    'menu_id' => 'footer-menu_2',
+    'depth' => 2,
+    'container' => false,
+    'menu_class' => 'menu menu-2',
+]);?>
             </div>
             <div class="col-12 col-md-3 copyright">
 
@@ -55,7 +62,8 @@
         (m[i].a = m[i].a || []).push(arguments)
     };
     m[i].l = 1 * new Date();
-    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k,
+        a)
 })
 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
@@ -85,7 +93,7 @@ gtag('config', 'UA-177779491-1');
 </script>
 
 
-<script src="<?php echo get_template_directory_uri(); ?>/assets/node_modules/share-buttons/dist/share-buttons.js?<?=time();?>"></script>
+<script src="//cdn.jsdelivr.net/npm/share-buttons/dist/share-buttons.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/dist/main.js?<?=time();?>"></script>
 
 <?php wp_footer();?>
--- page-kontakty.php
+++ page-kontakty.php
@@ -30,14 +30,21 @@ if (function_exists('yoast_breadcrumb')) {
                 <div class="row onmap">
                     <div class="onmap__box">
                         <ul>
-                            <li><a href="tel:8(812)363-61-11"><i class="fa fa-phone" aria-hidden="true"></i> 8(812)363-61-11, доб. 3429</a></li>
-                            <li><a href="mailto:info@dataslov.ru"><i class="fa fa-envelope" aria-hidden="true"></i> info@dataslov.ru</a></li>
-                            <li><i class="fa fa-map-marker" aria-hidden="true"></i> Санкт-Петербург, 1-я линия В.О., д. 26, к. 703</li>
-                            <li><a href="https://vk.com/dataslov" target="_blank"><i class="fa  fa-vk" aria-hidden="true"></i><span class="mt-1">VK<br /></span></a></li>
+                            <li><a href="tel:8(812)363-61-11"><i class="fa fa-phone" aria-hidden="true"></i>
+                                    8(812)363-61-11, доб. 3429</a></li>
+                            <li><a href="mailto:info@dataslov.ru"><i class="fa fa-envelope" aria-hidden="true"></i>
+                                    info@dataslov.ru</a></li>
+                            <li><i class="fa fa-map-marker" aria-hidden="true"></i> Санкт-Петербург, 1-я линия В.О., д.
+                                26, к. 703</li>
+                            <li><a href="https://vk.com/dataslov" target="_blank"><i class="fa  fa-vk"
+                                        aria-hidden="true"></i><span class="mt-1">VK<br /></span></a></li>
+                            <li><a href="https://t.me/dataslov" target="_blank"><i class="fa  fa-telegram"
+                                        aria-hidden="true"></i><span class="mt-1">Telegram<br /></span></a></li>
                         </ul>
                     </div>
                 </div>
-                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4e61a901760e1760d4b37990ec64f7875fd3c09435a0112b99b7ea284f456296&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true">
+                <script type="text/javascript" charset="utf-8" async
+                    src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4e61a901760e1760d4b37990ec64f7875fd3c09435a0112b99b7ea284f456296&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true">
                 </script>
             </div>
         </div>
